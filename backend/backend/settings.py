2f17720ead8465a7f4b0e675da6bd9a5cfde1c93
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-03-20 11:24:58 +0530

    *

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases #
 
 DATABASES = {
     'default': {
