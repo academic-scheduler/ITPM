4a7edd1d019cce3c7172f2d0e80def8edec2f09c
Hasintha Madushan <hasinthagunawardana2000@gmail.com>
2025-03-27 17:44:50 +0530

    update frontend

---
+++ frontend/src/views/smart-assistant/SmartAssistant.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+
+const SmartAssistant = () => {
+  return (
+    <div>
+      <h1>Smart Assistant</h1>
+    </div>
+  )
+}
+
+export default SmartAssistant
